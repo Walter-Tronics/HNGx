ed6f9e0f5e0d95196da48c84d6723d326c2062ee
Walter Tronics <waltertronics02@gmail.com>
2023-09-08 00:24:55 +0100

    fix: change time to current UTC in miliseconds

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 window.onload = () => {
-
     //selecting elements
     let day = document.querySelector('#day'),
     utc = document.querySelector('#utc');
 
+
+    const today = new Date();
+
     //function for getting the day of the week
     let getDay = () => {
-        const today = new Date(),
-        
         options = {
         weekday: 'long'
         };
-        
         return today.toLocaleDateString('en-US', options);
     }
     //appending the day value to the HTML
@@ -20,13 +19,7 @@ window.onload = () => {
 
     //function for getting the current UTC
     let getUTC = () => {
-        const date = new Date(),
-        hour = date.getUTCHours() ,
-        min = date.getUTCMinutes(),
-        sec = date.getUTCSeconds();
-        
-        return utc.innerText = `${hour < 10 ? '0'+hour : hour}:${min < 10 ? '0'+min : min}:${sec < 10 ? '0'+sec : sec} ${hour < 12 ? 'AM' : 'PM'}`;
+        return today.getUTCMilliseconds();
     }
-    
-    setInterval(getUTC, 1000);
+    utc.innerText = getUTC();
 }
